27eb58d043c1efbaf64b61ff3c32e29fccdcb99f
Caiofdrf <129322990+Caiofdrf@users.noreply.github.com>
2025-02-16 19:27:39 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
-nasm -f bin -o mybootloader.bin mybootloader.asm 
-nasm -f bin -o kernel.bin microkernel.asm 
+nasm -f bin -o bootloader.bin bootloader.asm 
+nasm -f bin -o kernel.bin kernel.asm 
+nasm -f bin -o composer.bin composer.asm 
+nasm -f bin -o texteditor.bin text_editor.asm 
 
 dd if=kernel.bin of=disk.img bs=512 seek=1 conv=notrunc
-dd if=mybootloader.bin of=disk.img bs=512 count=1 conv=notrunc
+dd if=composer.bin of=disk.img bs=512 seek=4 conv=notrunc
+dd if=texteditor.bin of=disk.img bs=512 seek=6 conv=notrunc
+dd if=bootloader.bin of=disk.img bs=512 count=1 conv=notrunc
 
 qemu-system-x86_64 -drive format=raw,file=disk.img
