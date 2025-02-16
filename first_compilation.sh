4cf9c3de4e73cdf8e27302e814e4bdc6ebf0b2d7
Caiofdrf <129322990+Caiofdrf@users.noreply.github.com>
2025-02-16 19:26:32 -0300

    Update first_compilation.sh

--- first_compilation.sh
+++ first_compilation.sh
@@ -5,6 +5,8 @@ clear
 # Compilação dos arquivos de bootloader e kernel
 nasm -f bin -o bootloader.bin bootloader.asm
 nasm -f bin -o kernel.bin kernel.asm
+nasm -f bin -o composer.bin composer.asm 
+nasm -f bin -o texteditor.bin text_editor.asm 
 
 dd if=/dev/zero of=disk.img bs=1M count=5
 
@@ -14,6 +16,8 @@ parted disk.img --script -- mkpart primary 1MiB 4MiB
 parted disk.img --script -- set 1 boot on
 
 dd if=kernel.bin of=disk.img bs=512 seek=1 conv=notrunc
+dd if=composer.bin of=disk.img bs=512 seek=4 conv=notrunc
+dd if=texteditor.bin of=disk.img bs=512 seek=6 conv=notrunc
 dd if=mybootloader.bin of=disk.img bs=512 count=1 conv=notrunc
 
 parted disk.img set 1 boot on
